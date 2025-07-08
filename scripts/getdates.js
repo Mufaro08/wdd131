177dcb6efe9f9f789a5f377d3c14fbfd16b57aaa
Mufaro <mufaromaz@gmail.com>
2025-07-08 22:57:50 +0200

    initial

---
+++ scripts/getdates.js
@@ -0,0 +1,12 @@
+
+const footer = document.querySelector("footer");
+const firstParagraph = footer.querySelector("p:nth-of-type(1)");
+const secondParagraph = footer.querySelector("p:nth-of-type(2)");
+
+
+const currentYear = new Date().getFullYear();
+const lastModified = document.lastModified;
+
+
+firstParagraph.textContent = `© ${currentYear} Mufaro Justice Tapera`;
+secondParagraph.textContent = `Last Modified: ${lastModified}`;

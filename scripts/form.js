02d413a8f5a8afd1435201938304396f29ee2048
Mufaro <mufaromaz@gmail.com>
2025-08-06 22:23:20 +0200

    fixes

--- scripts/form.js
+++ scripts/form.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-let count = localStorage.getItem('reviewCount') || 10;
+let count = localStorage.getItem('reviewCount') || 0;
     count++;
     localStorage.setItem('reviewCount', count);
     document.getElementById('reviewCount').textContent = count;
